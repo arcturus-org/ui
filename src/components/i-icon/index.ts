eef22a19a1b1663990424d325d2ffbede94c187c
ARCTURUS <1638330246@qq.com>
2022-06-13 21:39:41 +0800

    feat: support fontawesome

---
+++ src/components/i-icon/index.ts
@@ -0,0 +1,17 @@
+Component({
+  externalClasses: ['i-icon-class'],
+  properties: {
+    icon: String,
+    size: {
+      type: Number,
+      value: 20,
+    },
+  },
+  lifetimes: {
+    ready() {
+      if (this.data.icon === '') {
+        console.warn('图标类型不能为空');
+      }
+    },
+  },
+});
