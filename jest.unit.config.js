4d1df9b90ad0e4e73278adb7e08d1de0f9edbacd
ARCTURUS <1638330246@qq.com>
2022-06-13 16:54:15 +0800

    chore: use mpflow

---
+++ jest.unit.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: '@mpflow/plugin-unit-test',
+  bail: 1,
+  verbose: true,
+  testURL: 'https://jest.test',
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: [
+    '<rootDir>/src/**/__test(s)?__/**/*.(spec|test).{js,ts}',
+  ],
+  transform: {
+    '^.+\\.(j|t)s$': [
+      'babel-jest',
+      { configFile: require.resolve('./babel.config.js') },
+    ],
+  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,ts}',
+    '!**/__test(s)?__/**',
+  ],
+};
