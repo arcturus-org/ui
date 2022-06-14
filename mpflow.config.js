e6f8c700c0ab5ccaf75b8d04973c99099b4159ff
ARCTURUS <1638330246@qq.com>
2022-06-14 14:01:44 +0800

    chore: Change to a component library project

--- mpflow.config.js
+++ mpflow.config.js
@@ -1,13 +1,21 @@
 module.exports = {
   appId: 'wx139ead4836c519e2',
-  app: 'src/app',
-  compileType: 'miniprogram',
+  app: (mode) =>
+    mode === 'production' ? undefined : 'src/app',
+  pages: (mode) =>
+    mode === 'production'
+      ? ['src/components/index']
+      : undefined,
+  compileType: 'miniprogram', // 构建类型
   plugins: [
     '@mpflow/plugin-babel',
     '@mpflow/plugin-typescript',
     '@mpflow/plugin-css',
     '@mpflow/plugin-unit-test',
   ],
+  sourceMap: false,
+  sourceDir: 'src', // 源码目录
+  outputDir: 'dist', // 打包输出目录
   settings: {
     urlCheck: true,
     es6: true,
