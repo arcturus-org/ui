3673aa8990d1dea5439b95bbd8fd381fb7f7775b
ARCTURUS <1638330246@qq.com>
2022-07-18 23:53:51 +0800

    feat: [page] 首页路由

--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,5 +1,35 @@
 Page({
-    onShow() {
-        this.getTabBar().init();
-    },
-})
+  data: {
+    layout: [
+      {
+        label: '导航栏',
+        path: '',
+      },
+      {
+        label: '卡片',
+        path: '/pages/components/card/index',
+      },
+      {
+        label: '栅格',
+        path: '/pages/components/row/index',
+      },
+      {
+        label: 'grid',
+        path: '',
+      },
+      {
+        label: '标题',
+        path: '',
+      },
+    ],
+    display: [
+      {
+        label: '分割线',
+        path: '/pages/components/separator/index',
+      },
+    ],
+  },
+  onShow() {
+    this.getTabBar().init();
+  },
+});
