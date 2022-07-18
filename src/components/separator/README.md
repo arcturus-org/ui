720a9b5f5285704c57a30abe4971b663628a3a9e
ARCTURUS <1638330246@qq.com>
2022-07-18 23:19:31 +0800

    feat: [components] 分割线组件

---
+++ src/components/separator/README.md
@@ -0,0 +1,40 @@
+# 分割线组件
+
+## Usage
+
+### 横向分割
+
+```html
+<ice-separator></ice-separator>
+```
+
+### 纵向分割
+
+```html
+<ice-separator vertical></ice-separator>
+```
+
+### 内缩
+
+```html
+<ice-separator inset></ice-separator>
+```
+
+### 自定义内缩距离
+```html
+<ice-separator spaced="16px"></ice-separator>
+```
+
+### 设置文本
+
+```html
+<ice-cell label="到底啦"></ice-cell>
+```
+
+### 设置文本位置
+
+```html
+<ice-cell label="到底啦" position="center"></ice-cell>
+```
+
+> 有 "center" "right" "left" 三值可选
--- src/components/separator/index.ts
+++ src/components/separator/index.ts
@@ -9,5 +9,46 @@ Component({
       type: Boolean,
       value: false,
     },
+    spaced: String,
+    label: String, // 自定义文本
+    position: {
+      type: String,
+      value: 'center', // 文本位置
+    },
+  },
+  data: {
+    o: 0,
+  },
+  lifetimes: {
+    attached() {
+      const { position, label } = this.data;
+
+      if (label) {
+        this.changePosition(position);
+      }
+    },
+  },
+  methods: {
+    changePosition(position) {
+      let o;
+
+      switch (position) {
+        case 'center':
+          o = 5;
+          break;
+        case 'left':
+          o = 1;
+          break;
+        case 'right':
+          o = 9;
+      }
+
+      this.setData({ o });
+    },
+  },
+  observers: {
+    position: function (p) {
+      this.changePosition(p);
+    },
   },
 });
