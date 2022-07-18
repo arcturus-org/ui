12a4d91cbdc0bfe3d8127c2b1955aa3f6771d812
ARCTURUS <1638330246@qq.com>
2022-07-18 16:04:09 +0800

    feat: 闭包实现参数复用

--- src/components/utils/navInfo.ts
+++ src/components/utils/navInfo.ts
@@ -1,5 +1,11 @@
+interface nav {
+  navHeight: number;
+  statusBarHeight: number;
+  capsuleLeft: number;
+}
+
 // 获取导航栏信息
-export default function () {
+function navInfo(): nav {
   const {
     top: capsuleTop,
     bottom: capsuleBottom,
@@ -10,11 +16,24 @@ export default function () {
     statusBarHeight, // 状态栏高度
   } = wx.getSystemInfoSync();
 
-  const navHeight: number = capsuleBottom + capsuleTop - 2 * statusBarHeight; // 导航栏高度
+  const navHeight: number =
+    capsuleBottom + capsuleTop - 2 * statusBarHeight; // 导航栏高度
 
   return {
     navHeight, // 导航栏高度
     statusBarHeight,
     capsuleLeft, // 胶囊左边位置
   };
+}
+
+export default function useNavInfo() {
+  let n;
+
+  return (function (): nav {
+    if (n) {
+      return n;
+    } else {
+      return navInfo();
+    }
+  })();
 }
