e2e3655651efb216404eada1eb492e527f08a1ab
ARCTURUS <1638330246@qq.com>
2022-07-18 15:19:06 +0800

    feat: [components] 分割组件

---
+++ src/components/separator/index.ts
@@ -0,0 +1,13 @@
+// 这是一条分割线
+Component({
+  properties: {
+    inset: {
+      type: Boolean,
+      value: false,
+    },
+    vertical: {
+      type: Boolean,
+      value: false,
+    },
+  },
+});
