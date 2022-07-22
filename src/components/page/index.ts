aa168241d7f8d329812e33e438191ddcf562f9c3
ARCTURUS <1638330246@qq.com>
2022-07-22 13:04:26 +0800

    feat: [components] 完善page组件

--- src/components/page/index.ts
+++ src/components/page/index.ts
@@ -1,3 +1,5 @@
+import useSafeHeight from '../utils/safeArea';
+
 Component({
   // 启用多 slot 支持
   options: {
@@ -9,21 +11,29 @@ Component({
       linked: function (
         target: WechatMiniprogram.Component.TrivialInstance
       ) {
-        const { navHeight, statusBarHeight, occupy } =
-          target.data;
+        const { navHeight, occupy } = target.data;
+        const { height } = this.data;
 
         if (occupy) {
           this.setData({
-            navHeight: navHeight + statusBarHeight,
+            height: height - navHeight,
           });
         }
       },
     },
-    '../tabbar/index': {
-      type: 'child',
-    },
   },
   data: {
-    navHeight: 0,
+    height: 0,
+  },
+  lifetimes: {
+    attached() {
+      const tabbar = this.getTabBar();
+
+      if (tabbar) {
+        const height = useSafeHeight();
+        // 当前页面存在 tabbar
+        this.setData({ height });
+      }
+    },
   },
 });
